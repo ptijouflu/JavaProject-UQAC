9f078513e6eee8650280151795f57467a061feb6
ptijouflu <rivetvictor@gmail.com>
2022-02-27 01:36:02 +0100

    Create Sport.java

---
+++ src/src/ca/uqac/inf334/Sport.java
@@ -0,0 +1,27 @@
+package ca.uqac.inf334;
+
+import java.util.*;
+
+public class Sport {
+    String name;
+    ArrayList<String> students = new ArrayList<String>();
+
+
+    /**
+     * Constructor
+     */
+    public void Sport(String name){
+        this.name = name;
+
+    }
+
+    /**
+     * Get sport list
+     */
+    public void SportList(){
+        for (String i :students){
+            System.out.println(students);
+        }
+    }
+
+}
