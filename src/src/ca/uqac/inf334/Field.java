309a9472e6e07bb52b34299bfcafab2b5a5477aa
sabarthazo <sabarthazo@gmail.com>
2022-02-26 19:13:29 -0500

    Create Field.java

---
+++ src/src/ca/uqac/inf334/Field.java
@@ -0,0 +1,24 @@
+package ca.uqac.inf334;
+
+import java.util.ArrayList;
+
+public class Field {
+    String name;
+    ArrayList<String> courses = new ArrayList<String>();
+
+    /**
+     * Constructor
+     */
+    public void Field(String name){
+        this.name = name;
+    }
+
+    /**
+     * Get courses of the field
+     */
+    public void GetCoursesList(){
+        for (String i: courses){
+            System.out.println(i);
+        }
+    }
+}
